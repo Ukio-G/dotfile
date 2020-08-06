0c9f89160a9431030f340efa53db71126aea0f53
Ваше Имя <you@example.com>
2020-08-06 23:51:30 +0300

    update configs + add basic install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# require vim xorg-xrdb rofi i3wm xbindkeys zsh (and oh-my-zsh extension) zsh-autosuggestions zsh-syntax-highlighting
+
+cp -r .local .xres .vim .config ~
+cp .vimrc .Xresources .xinitrc .xbindkeysrc .zshrc~
